b104d5c9d98a7ef9821a9dde58bcae6cbd280369
rememberlenny <lkbgift@gmail.com>
2013-07-19 12:01:06 -0500

    Added new test for titles and added the title symbol

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe 'Static Pages' do
       expect(page).to have_content('Home Page')
     end
 
+    it "should have the title 'Ruby on Rails Application | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Application | Home')
+    end
+
   end
 
   describe 'Help Page' do
@@ -17,6 +22,11 @@ describe 'Static Pages' do
       expect(page).to have_content('Help')
     end
 
+    it "should have the title 'Ruby on Rails Application | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Application | Help')
+    end
+
   end
 
   describe 'About Page' do
@@ -24,6 +34,11 @@ describe 'Static Pages' do
     it "should have the content 'About Page'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have the title 'Ruby on Rails Application | About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Application | About')
     end
 
   end
