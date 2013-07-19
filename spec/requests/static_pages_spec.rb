23cef9dd863fcbc80ccd23712e507d3f0c2aa967
rememberlenny <lkbgift@gmail.com>
2013-07-19 12:12:00 -0500

    refactored tests to let base_title substitute title text

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'Static Pages' do
+
+  let(:base_title) {"Ruby on Rails Application"}
+
   describe 'Home Page' do
 
     it "should have the content 'Home Page'" do
@@ -10,7 +13,7 @@ describe 'Static Pages' do
 
     it "should have the title 'Ruby on Rails Application | Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Application | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -24,7 +27,21 @@ describe 'Static Pages' do
 
     it "should have the title 'Ruby on Rails Application | Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Application | Help')
+      expect(page).to have_title("#{base_title} | Help")
+    end
+
+  end
+
+  describe 'Contact Page' do
+
+    it "should have the content 'Contact Page'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Ruby on Rails Application | Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
   end
@@ -38,7 +55,7 @@ describe 'Static Pages' do
 
     it "should have the title 'Ruby on Rails Application | About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Application | About')
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end
